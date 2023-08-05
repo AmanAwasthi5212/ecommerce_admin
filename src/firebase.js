384265c0cf78c2d42bffd5f0bc82c421118a706d
AmanAwasthi5212 <202212077@daiict.ac.in>
2023-08-05 17:28:42 +0530

    Add existing project files prior to the push to GitHub.

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwPC0UtRT26S0WUXS9F_BjSTHXDlrkJ2E",
+  authDomain: "easyshop-12059.firebaseapp.com",
+  projectId: "easyshop-12059",
+  storageBucket: "easyshop-12059.appspot.com",
+  messagingSenderId: "300721761050",
+  appId: "1:300721761050:web:24922ca56fb3c88e851aa3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
